472b6f5429573fb19be5cfed4fbd9b54a5d3deac
asst <asst93i@mail.ru>
2020-11-13 02:53:58 -0600

    9.1 and pytest.ini was changed

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -61,12 +61,19 @@ access_config_2 = {
 
 
 def generate_access_config(intf_vlan_mapping, access_template):
-    """
-    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
-        {'FastEthernet0/12':10,
-         'FastEthernet0/14':11,
-         'FastEthernet0/16':17}
-    access_template - список команд для порта в режиме access
+    res = []
+    for intf,vlan in intf_vlan_mapping.items() :
+        res.append(f"interface {intf}")
+        for command in access_template :
+            if "vlan" in command:
+                    res.append(command + " " + str(vlan))
+            else :
+                res.append(command)
 
-    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
-    """
+    return res
+
+
+
+
+r = generate_access_config(access_config, access_mode_template)
+print(r)
